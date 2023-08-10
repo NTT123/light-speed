979899a63bc36b63bb440142c15819934d1ae6de
ntt123 <xcodevn@gmail.com>
2023-08-10 19:48:27 +0800

    add evaluation

--- models.py
+++ models.py
@@ -501,10 +501,10 @@ class SynthesizerTrn(nn.Module):
         self,
         x,
         x_lengths,
+        y_lengths,
+        attn,
         sid=None,
         noise_scale=1,
-        length_scale=1,
-        noise_scale_w=1.0,
         max_len=None,
     ):
         x, m_p, logs_p, x_mask = self.enc_p(x, x_lengths)
@@ -513,23 +513,13 @@ class SynthesizerTrn(nn.Module):
         else:
             g = None
 
-        if self.use_sdp:
-            logw = self.dp(x, x_mask, g=g, reverse=True, noise_scale=noise_scale_w)
-        else:
-            logw = self.dp(x, x_mask, g=g)
-        w = torch.exp(logw) * x_mask * length_scale
-        w_ceil = torch.ceil(w)
-        y_lengths = torch.clamp_min(torch.sum(w_ceil, [1, 2]), 1).long()
-        y_mask = torch.unsqueeze(commons.sequence_mask(y_lengths, None), 1).to(
+        y_mask = torch.unsqueeze(commons.sequence_mask(y_lengths, attn.shape[1]), 1).to(
             x_mask.dtype
         )
-        attn_mask = torch.unsqueeze(x_mask, 2) * torch.unsqueeze(y_mask, -1)
-        attn = commons.generate_path(w_ceil, attn_mask)
-
-        m_p = torch.matmul(attn.squeeze(1), m_p.transpose(1, 2)).transpose(
+        m_p = torch.matmul(attn, m_p.transpose(1, 2)).transpose(
             1, 2
         )  # [b, t', t], [b, t, d] -> [b, d, t']
-        logs_p = torch.matmul(attn.squeeze(1), logs_p.transpose(1, 2)).transpose(
+        logs_p = torch.matmul(attn, logs_p.transpose(1, 2)).transpose(
             1, 2
         )  # [b, t', t], [b, t, d] -> [b, d, t']
 
--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm.auto import tqdm
 
 import commons
+import utils
 from losses import discriminator_loss, feature_loss, generator_loss, kl_loss
 from mel_processing import mel_spectrogram_torch, spec_to_mel_torch
 from models import MultiPeriodDiscriminator, SynthesizerTrn
@@ -23,7 +24,7 @@ parser = ArgumentParser()
 parser.add_argument("--config", type=str, default="config.json")
 parser.add_argument("--tfdata", type=str, default="data/tfdata")
 parser.add_argument("--log-dir", type=Path, default="logs")
-parser.add_argument("--ckpt-dir", type=Path, default="logs")
+parser.add_argument("--ckpt-dir", type=Path, default="ckpts")
 parser.add_argument("--batch-size", type=int, default=16)
 parser.add_argument("--compile", action="store_true", default=False)
 parser.add_argument("--device", type=str, default="cuda")
@@ -64,7 +65,7 @@ with open(FLAGS.config, "rb") as f:
 torch.manual_seed(hps.train.seed)
 
 train_writer = SummaryWriter(FLAGS.log_dir / "train", flush_secs=30)
-val_writer = SummaryWriter(FLAGS.log_dir / "val", flush_secs=30)
+test_writer = SummaryWriter(FLAGS.log_dir / "test", flush_secs=30)
 
 net_g = SynthesizerTrn(
     256,
@@ -86,62 +87,127 @@ optim_d = torch.optim.AdamW(
     eps=hps.train.eps,
 )
 
+scheduler_g = torch.optim.lr_scheduler.ExponentialLR(optim_g, gamma=hps.train.lr_decay)
+scheduler_d = torch.optim.lr_scheduler.ExponentialLR(optim_d, gamma=hps.train.lr_decay)
+
 if compile:
     # net_g = torch.compile(net_g)
     net_d = torch.compile(net_d)
 
 
-scheduler_g = torch.optim.lr_scheduler.ExponentialLR(optim_g, gamma=hps.train.lr_decay)
-scheduler_d = torch.optim.lr_scheduler.ExponentialLR(optim_d, gamma=hps.train.lr_decay)
+ckpts = sorted(FLAGS.ckpt_dir.glob("ckpt_*.pth"))
+if len(ckpts) > 0:
+    # last ckpt
+    ckpt = ckpts[-1]
+    print(f"loading checkpoint {ckpt}")
+    ckpt = torch.load(ckpt, map_location=device)
+    net_g.load_state_dict(ckpt["net_g"])
+    net_d.load_state_dict(ckpt["net_d"])
+    optim_g.load_state_dict(ckpt["optim_g"])
+    optim_d.load_state_dict(ckpt["optim_d"])
+    scheduler_g.load_state_dict(ckpt["scheduler_g"])
+    scheduler_d.load_state_dict(ckpt["scheduler_d"])
+    step = ckpt["step"]
+    scaler.load_state_dict(ckpt["scaler"])
+    _epoch = ckpt.get("epoch", 0)
+else:
+    step = 0
+    _epoch = 0
+
 
 net_g.train()
 net_d.train()
-step = 0
 
-for epoch in range(100_000):
+
+def prepare_batch(batch):
+    x = torch.from_numpy(batch["phone_idx"]).to(device, non_blocking=True).long()
+    x_lengths = (
+        torch.from_numpy(batch["phone_length"]).to(device, non_blocking=True).long()
+    )
+    spec = (
+        torch.from_numpy(batch["spec"])
+        .to(device, non_blocking=True)
+        .swapaxes(-1, -2)
+        .float()
+    )
+    spec_lengths = (
+        torch.from_numpy(batch["spec_length"]).to(device, non_blocking=True).long()
+    )
+    y = torch.from_numpy(batch["wav"]).to(device, non_blocking=True).float()[:, None, :]
+    y_lengths = (
+        torch.from_numpy(batch["wav_length"]).to(device, non_blocking=True).long()
+    )
+    duration = (
+        torch.from_numpy(batch["phone_duration"]).to(device, non_blocking=True).float()
+    )
+    end_time = torch.cumsum(duration, dim=-1)
+    start_time = end_time - duration
+    start_frame = (
+        start_time * hps.data.sampling_rate / hps.data.hop_length / 1000
+    ).int()
+    end_frame = (end_time * hps.data.sampling_rate / hps.data.hop_length / 1000).int()
+    pos = torch.arange(0, spec.shape[-1], device=spec.device)
+    attn = torch.logical_and(
+        pos[None, :, None] >= start_frame[:, None, :],
+        pos[None, :, None] < end_frame[:, None, :],
+    ).float()
+    return x, x_lengths, spec, spec_lengths, y, y_lengths, attn
+
+
+def evaluate(step):
+    net_g.eval()
+    ds = load_tfdata(FLAGS.tfdata, "test", FLAGS.batch_size, 0)
+    with torch.no_grad():
+        batch = next(ds.as_numpy_iterator())
+        x, x_lengths, spec, spec_lengths, y, y_lengths, attn = prepare_batch(batch)
+        # remove else
+        x = x[:1]
+        x_lengths = x_lengths[:1]
+        spec = spec[:1]
+        spec_lengths = spec_lengths[:1]
+        y = y[:1]
+        y_lengths = y_lengths[:1]
+        print("attn ", attn.shape)
+        attn = attn[:1]
+        y_hat, attn, *_ = net_g.infer(x, x_lengths, spec_lengths, attn, max_len=1000)
+        y_hat_lengths = y_lengths
+
+        mel = spec_to_mel_torch(
+            spec,
+            hps.data.filter_length,
+            hps.data.n_mel_channels,
+            hps.data.sampling_rate,
+            hps.data.mel_fmin,
+            hps.data.mel_fmax,
+        )
+        y_hat_mel = mel_spectrogram_torch(
+            y_hat.squeeze(1).float(),
+            hps.data.filter_length,
+            hps.data.n_mel_channels,
+            hps.data.sampling_rate,
+            hps.data.hop_length,
+            hps.data.win_length,
+            hps.data.mel_fmin,
+            hps.data.mel_fmax,
+        )
+    gen_mel = utils.plot_spectrogram_to_numpy(y_hat_mel[0].cpu().numpy())
+    gt_mel = utils.plot_spectrogram_to_numpy(mel[0].cpu().numpy())
+    test_writer.add_audio(
+        "gt/audio", y[0, :, : y_lengths[0]], step, hps.data.sampling_rate
+    )
+    test_writer.add_audio(
+        "gen/audio", y_hat[0, :, : y_hat_lengths[0]], step, hps.data.sampling_rate
+    )
+    test_writer.add_image("mel", gen_mel, step, dataformats="HWC")
+    test_writer.add_image("mel_gt", gt_mel, step, dataformats="HWC")
+    net_g.train()
+
+
+for epoch in range(_epoch + 1, 100_000):
     ds = load_tfdata(FLAGS.tfdata, "train", FLAGS.batch_size, epoch)
     for batch in tqdm(ds.as_numpy_iterator()):
         step = step + 1
-        x = torch.from_numpy(batch["phone_idx"]).to(device, non_blocking=True).long()
-        x_lengths = (
-            torch.from_numpy(batch["phone_length"]).to(device, non_blocking=True).long()
-        )
-        spec = (
-            torch.from_numpy(batch["spec"])
-            .to(device, non_blocking=True)
-            .swapaxes(-1, -2)
-            .float()
-        )
-        spec_lengths = (
-            torch.from_numpy(batch["spec_length"]).to(device, non_blocking=True).long()
-        )
-        y = (
-            torch.from_numpy(batch["wav"])
-            .to(device, non_blocking=True)
-            .float()[:, None, :]
-        )
-        y_lengths = (
-            torch.from_numpy(batch["wav_length"]).to(device, non_blocking=True).long()
-        )
-        duration = (
-            torch.from_numpy(batch["phone_duration"])
-            .to(device, non_blocking=True)
-            .float()
-        )
-        end_time = torch.cumsum(duration, dim=-1)
-        start_time = end_time - duration
-        start_frame = (
-            start_time * hps.data.sampling_rate / hps.data.hop_length / 1000
-        ).int()
-        end_frame = (
-            end_time * hps.data.sampling_rate / hps.data.hop_length / 1000
-        ).int()
-        pos = torch.arange(0, spec.shape[-1], device=spec.device)
-        attn = torch.logical_and(
-            pos[None, :, None] >= start_frame[:, None, :],
-            pos[None, :, None] < end_frame[:, None, :],
-        )
-
+        x, x_lengths, spec, spec_lengths, y, y_lengths, attn = prepare_batch(batch)
         with ctx:
             (
                 y_hat,
@@ -151,7 +217,7 @@ for epoch in range(100_000):
                 x_mask,
                 z_mask,
                 (z, z_p, m_p, logs_p, m_q, logs_q),
-            ) = net_g(x, x_lengths, attn.float(), spec, spec_lengths)
+            ) = net_g(x, x_lengths, attn, spec, spec_lengths)
 
         mel = spec_to_mel_torch(
             spec,
@@ -224,9 +290,11 @@ for epoch in range(100_000):
         train_writer.add_scalar("grad_norm_g", grad_norm_g, global_step=step)
 
         if step % FLAGS.ckpt_interval == 0:
+            evaluate(step)
             torch.save(
                 {
                     "step": step,
+                    "epoch": epoch,
                     "net_g": net_g.state_dict(),
                     "net_d": net_d.state_dict(),
                     "scaler": scaler.state_dict(),
